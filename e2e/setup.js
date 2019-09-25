8a83e639239b7a957dcb8c9fcf8296de9415730a
Maeve Griffin <griffinmaeve@gmail.com>
2019-09-25 15:55:31 -0700

    marty fix

--- e2e/setup.js
+++ e2e/setup.js
@@ -3,8 +3,7 @@ const TEST_DB_URL = 'mongodb://localhost:27017/db';
 const mongoose = require('mongoose');
 
 beforeAll(() => {
-  console.log('is this before');
-  return connect(TEST_DB_URL);
+  return connect(TEST_DB_URL, { log: false });
 });
 
 afterAll(() => {
